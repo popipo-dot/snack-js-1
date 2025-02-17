549b0eeb0e4a88821ac16e39159d06167d101036
popipo-dot <76meonie@gmail.com>
2025-02-18 00:56:59 +0100

    exercise 6 done

---
+++ esercizio 6/esercizio_6.js
@@ -0,0 +1,16 @@
+// BONUS - Esercizio 6
+// Segui le istruzioni qui sotto. Se necessario, sfrutta il destructuring.
+
+const studenti = [
+  { nome: "Anna", eta: 17 },
+  { nome: "Marco", eta: 22 },
+  { nome: "Laura", eta: 19 },
+  { nome: "Giorgio", eta: 16 }
+];
+
+// 1. Filtra solo gli studenti maggiorenni e salva il risultato in una variabile
+// 2. Per ogni maggiorenne stampa in console "Lo studente <nome-studente> è maggiorenne."
+
+studenti.forEach ((studente) => {
+    if ()
+})
